e54e7d8339bdc2a32b16ec750131b625dcabd7df
Boris Skert <boris.skert@gmail.com>
2020-04-03 10:35:30 +0200

    Clean up project

--- src/main/java/de/borisskert/springboot/webflux/Application.java
+++ src/main/java/de/borisskert/springboot/webflux/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWebfluxExampleApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebfluxExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
