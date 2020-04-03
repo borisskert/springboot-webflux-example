86ac94c1d83493dfd99bbdd93444ba2f72a02d4f
Boris Skert <boris.skert@gmail.com>
2020-04-03 10:32:33 +0200

    Create project with Spring Initializr

---
+++ src/test/java/de/borisskert/springboot/webflux/SpringBootWebfluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borisskert.springboot.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
